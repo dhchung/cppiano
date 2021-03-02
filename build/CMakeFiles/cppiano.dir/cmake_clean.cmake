b74d90153686caff6a87caf7b012310a9e2e5d75
Dongha Chung <haetae.dongha@gmail.com>
2021-03-02 23:57:06 +0900

    first commit

---
+++ build/CMakeFiles/cppiano.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cppiano.dir/src/main.cpp.o"
+  "cppiano"
+  "cppiano.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cppiano.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
